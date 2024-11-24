1e2b733f14c3d503a0506e130910cdecc2f01aa1
Dr Rafael Bailo <Dr.Rafael.Bailo@gmail.com>
2024-11-24 17:43:44 +0100

    test build

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,8 @@ using HTMLBuilder
 using SafeTestsets, Test
 
 @testset "HTMLBuilder.jl" begin
-  for test ∈ ["aqua", "format", "HTMLElements", "parse_HTML", "utils"]
+  for test ∈
+      ["aqua", "build_site", "format", "HTMLElements", "parse_HTML", "utils"]
     @eval begin
       @safetestset $test begin
         include($test * ".jl")
